f94e641ef5a909e73682103a399cce848e1b2f30
sanketmaru <sanketmaru@gmail.com>
2019-12-23 16:54:19 +0530

    pricing component added

---
+++ src/app/pricing/pricing.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-pricing-component',
+  templateUrl: './pricing.component.html',
+  styleUrls: [ './pricing.component.scss' ]
+})
+export class PricingComponent  {
+  name = 'Angular';
+}
